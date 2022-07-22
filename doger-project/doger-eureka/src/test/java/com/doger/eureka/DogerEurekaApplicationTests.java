9377e7b69491fd2e1c727f25112b71afcb357d8f
tom <tom@doger.com>
2022-07-22 15:25:05 +0800

    add eureka service

---
+++ doger-project/doger-eureka/src/test/java/com/doger/eureka/DogerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doger.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogerEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
