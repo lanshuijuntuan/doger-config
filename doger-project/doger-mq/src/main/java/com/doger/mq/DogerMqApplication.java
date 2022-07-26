519b4cc282742ec0f6a60e7c1cdf7420546a4e38
tom <tom@doger.com>
2022-07-26 13:55:38 +0800

    添加配置文件

---
+++ doger-project/doger-mq/src/main/java/com/doger/mq/DogerMqApplication.java
@@ -0,0 +1,13 @@
+package com.doger.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogerMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogerMqApplication.class, args);
+    }
+
+}
