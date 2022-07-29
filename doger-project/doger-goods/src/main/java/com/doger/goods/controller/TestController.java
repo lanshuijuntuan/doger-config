58cb78b5cba1b5e80ed592ef768030faa4826bee
tom <tom@doger.com>
2022-07-29 18:03:09 +0800

    RabbitRpc

--- doger-project/doger-goods/src/main/java/com/doger/goods/controller/TestController.java
+++ doger-project/doger-goods/src/main/java/com/doger/goods/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.doger.goods.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 
 @Log4j2
+@RefreshScope
 @Controller
 @RequestMapping("test")
 public class TestController {
@@ -28,13 +32,24 @@ public class TestController {
     @GetMapping("printAppName")
     @ResponseBody
     public String printAppName(){
-        String instanceId=serviceRegistration.getInstanceId();
         String host=serviceRegistration.getHost();
         Integer port=serviceRegistration.getPort();
         String repoStr= host+":"+port+"\n\r"+appName+":"+envProfile;
         log.info(repoStr);
         return repoStr;
     }
+
+    @Resource
+    private Environment environment;
+
+    @GetMapping("printEnvName")
+    @ResponseBody
+    public String printEnvName(){
+        String repoStr= environment.getProperty("appName")+":"+environment.getProperty("env.profile");
+        log.info(repoStr);
+        return repoStr;
+    }
+
 
 
 }
