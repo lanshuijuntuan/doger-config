44616039d1cdd7aa50d6e82282e913244a4c45f2
tom <tom@doger.com>
2022-08-01 18:03:29 +0800

    74

---
+++ doger-project/doger-stream/src/main/java/com/doger/stream/DogerStreamApplication.java
@@ -0,0 +1,13 @@
+package com.doger.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogerStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogerStreamApplication.class, args);
+    }
+
+}
---
+++ doger-project/doger-stream/src/test/java/com/doger/stream/DogerStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doger.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogerStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
